5607d0cc043b01489a47397f405e127baddc0da5
adre <84japko@gmail.com>
2019-10-12 10:46:55 +0200

    Adding first file

---
+++ Simplecode.java
@@ -0,0 +1,11 @@
+public class Simplecode {
+
+	public static void main (String[] args)  {
+		int x = 3;
+		String name = "Number";
+		x = x * 17;
+		System.out.println("x is " + x);
+		//this is a comment
+	}
+
+}
